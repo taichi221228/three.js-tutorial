209bb4ee307396105791eef37682a652a9dae844
たいち ひ <taichi221228@icloud.com>
2024-12-28 17:53:20 +0900

    feat: Add basic THREE.js rendering setup with rotating cube
    Integrated a THREE.js canvas rendering a rotating cube into the app.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,46 @@
-import './style.css'
+import "./style.css";
+import * as THREE from "three";
+
+const { width, height } = {
+	width: 960,
+	height: 540,
+};
+
+const { app, canvas } = (() => {
+	const app = document.getElementById("app");
+
+	return { app, canvas: app.querySelector("canvas") };
+})();
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+renderer.setSize(width, height);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, width / height, 1);
+camera.position.set(0, 0, 1000);
+
+const geometry = new THREE.BoxGeometry(200, 200, 200);
+const material = new THREE.MeshStandardMaterial({ color: 0xffd700 });
+const box = new THREE.Mesh(geometry, material);
+scene.add(box);
+
+const light = new THREE.DirectionalLight(0xffffff);
+light.intensity = 2; // 光の強さを倍に
+light.position.set(1, 1, 1); // ライトの方向
+scene.add(light);
+
+renderer.render(scene, camera);
+
+tick();
+
+function tick() {
+	requestAnimationFrame(tick);
+
+	box.rotation.x += 0.01;
+	box.rotation.y += 0.01;
+
+	renderer.render(scene, camera);
+}
