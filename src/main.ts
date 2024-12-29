68cff521c77b6aaef8a003854399e4b2b829fb30
たいち ひ <taichi221228@icloud.com>
2024-12-29 11:25:00 +0900

    feat: Reduce directional light intensity in the scene.
    Removed an overly high intensity setting for the directional light to achieve better lighting balance.

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,6 @@ const box = new THREE.Mesh(geometry, material);
 scene.add(box);
 
 const light = new THREE.DirectionalLight(0xffffff);
-light.intensity = 2;
 light.position.set(1, 1, 1);
 scene.add(light);
 
